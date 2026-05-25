eb217897803203f5bf09f9ccc9c5d2234dd69442
Akash <akasha7207@gmail.com>
2026-05-25 20:16:33 +0530

    fixed middleware path

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const authMiddleware = require("../backend/middleware/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const {
     createProject,
